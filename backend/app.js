f5efaa2d7d854505621d855e80b528a4b7f7528e
MiniThought17 <felixomwenga441@gmail.com>
2023-06-12 20:44:19 +0300

    Backend changes

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ import userRoute from './routes/user-routes'
 import adminRoute from './routes/admin-routes'
 import movieRoute from './routes/movie-routes'
 import bookingRoute from './routes/booking-routes'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express()
 
 //middleware
+app.use(cors())
 app.use(express.json())
 app.use('/users',userRoute)
 app.use('/admin', adminRoute)
