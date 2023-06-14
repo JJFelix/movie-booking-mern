366ccd051e206e2bb4ba3cfc654c52f5dafa5e73
MiniThought17 <felixomwenga441@gmail.com>
2023-06-14 15:07:03 +0300

    Api helpers

--- movie-booking-frontend/src/api-helpers/api-helpers.jsx
+++ movie-booking-frontend/src/api-helpers/api-helpers.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import mongoose from 'mongoose'
 
 export const getAllMovies = async () => {
     const res = await axios
@@ -76,4 +77,84 @@ export const newBooking = async (data) =>{
     const resData = res.data
     return resData
 
+}
+
+export const getUserBooking  = async () =>{
+    const id = localStorage.getItem("userId")
+    const res = await axios
+        .get(`/users/bookings/${id}`)
+        .catch((err)=> console.log(err))
+
+    if(res.status !== 200 ){
+        return console.log("Could not get bookings of user")
+    }
+
+    const resData = await res.data
+    return resData
+}
+
+export const getOneUserDetails = async () =>{
+    const id = localStorage.getItem("userId") // || localStorage.getItem("adminId")
+    // const id = new mongoose.Types.ObjectId(userId)
+    const res = await axios
+        .get(`/users/${id}`)
+        .catch((err)=>console.log(err))
+
+    if(res.status !==200){
+        return console.log(`Could not find user with id ${id}`)
+    }
+
+    const resData = await res.data
+    return resData
+}
+
+export const deleteBooking = async (id)=>{
+    const res = await axios
+        .delete(`/bookings/${id}`)
+        .catch((err)=>console.log(err))
+
+    if(res.status !==200){
+        return console.log("Could  not delete booking")
+    }
+    
+    const resData = res.data
+    return resData
+}
+
+export const addMovie = async(data) =>{
+    const res = await axios
+        .post('/movies', {
+            title:data.title,
+            description:data.description,
+            releaseDate:data.releaseDate,
+            posterUrl:data.posterUrl,
+            featured:data.featured,
+            actors:data.actors,
+            admin:new mongoose.Types.ObjectId(localStorage.getItem("adminId"))
+        },{
+            headers:{
+                Authorization:`Bearer ${localStorage.getItem("token")}`
+            }
+        }).catch(err=>console.error(err))
+
+        if(res.status !=200){
+            return console.log("Could not add movie")
+        }
+
+        const resData = await res.data
+        return resData
+}
+
+export const getAdminById = async()=>{
+    const adminId = localStorage.getItem("adminId")
+
+    const res = await axios.get(`/admin/${adminId}`)
+    .catch(err=>console.error(err))
+
+    if(res.status !== 200){
+        return console.log(`Could not get admin with id ${adminId}`)
+    }
+
+    const resData = await res.data
+    return resData
 }
