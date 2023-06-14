14d8a51bed4f7dbffd8fd5a545739ef946608cee
MiniThought17 <felixomwenga441@gmail.com>
2023-06-14 15:11:30 +0300

    Main file

--- movie-booking-frontend/src/App.jsx
+++ movie-booking-frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from "./components/Navbar"
-import Admin from "./components/admin/Admin"
+import Admin from "./components/auth/Admin"
 import Homepage from "./components/homepage/Homepage"
 import Movies from "./components/movies/Movies"
 import Auth from "./components/auth/Auth"
+import UserProfile from './components/profile/UserProfile'
 import Booking from './components/bookings/Booking'
 import { useDispatch, useSelector } from 'react-redux'
 import { useEffect } from 'react'
 import { adminActions, userActions } from './store'
+import AddMovie from './components/movies/AddMovie'
+import AdminProfile from './components/profile/AdminProfile'
 
 // import viteLogo from '/vite.svg'
 function App() {
@@ -31,9 +34,27 @@ function App() {
           <Routes>
             <Route path='/' element={<Homepage />} />
             <Route path='/movies' element={<Movies />} />
-            <Route path='/admin' element={<Admin />} />
-            <Route path='/auth' element={<Auth />} />
-            <Route path='/booking/:id' element={<Booking />}></Route>
+
+            {!isUserLoggedIn && !isAdminLoggedIn &&
+              <>
+                <Route path='/admin' element={<Admin />} />
+                <Route path='/auth' element={<Auth />} />
+              </>
+            }
+
+            {isUserLoggedIn && !isAdminLoggedIn &&
+              <>
+                <Route path='/profile' element={<UserProfile />} />
+                <Route path='/booking/:id' element={<Booking />}/>              
+              </>              
+            }
+
+            {isAdminLoggedIn && !isUserLoggedIn &&
+              <>
+                <Route path='/add' element={<AddMovie />}/>
+                <Route path='/adminProfile' element={<AdminProfile />} /> 
+              </>
+            }
           </Routes>
         </section>
         {/*Homepage */}
